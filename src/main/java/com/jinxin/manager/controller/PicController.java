4b752ac3d9b53eca2e966756b554aef132278109
ZaneyYe <mia5121@163.com>
2017-08-15 19:14:08 +0800

    pic upload

--- src/main/java/com/jinxin/manager/controller/PicController.java
+++ src/main/java/com/jinxin/manager/controller/PicController.java
@@ -1,12 +1,10 @@
 package com.jinxin.manager.controller;
 
 import com.jinxin.manager.enumkit.Constants;
-import com.jinxin.manager.enumkit.StateInfo;
 import com.jinxin.manager.service.PicService;
 import com.jinxin.manager.vo.ImgInfoVo;
 import com.jinxin.manager.vo.PageInfo;
 import com.jinxin.manager.vo.RequestPage;
-import com.jinxin.manager.vo.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
@@ -55,35 +53,26 @@ public class PicController extends BaseController{
 	 * @param file
 	 */
 	@RequestMapping(value = "/uploadPic.do", method = RequestMethod.POST)
-	@ResponseBody
-	public ResponseEntity<StateInfo> uploadPic(HttpServletRequest request,
-											   @RequestParam("desc") String desc,
-											   @RequestParam("upFile") MultipartFile file) {
+	public void uploadPic(HttpServletRequest request,
+						  @RequestParam("desc") String desc,
+						  @RequestParam("upFile") MultipartFile file) {
 		LOGGER.info("pic uploading, {}, {}....", desc, file.getOriginalFilename());
-		ResponseEntity<StateInfo> entity = new ResponseEntity<>();
 		if (!file.isEmpty()) {
 			//上传文件
 			String fileName = file.getOriginalFilename(); //文件名
-			String contextPath = request.getContextPath();
-			LOGGER.info("contextPath:: {}", contextPath);
 			String descPath = Constants.localUploadPath + fileName; //目的文件
 			try {
 				FileCopyUtils.copy(file.getBytes(), new File(descPath));
 			} catch (IOException e) {
 				e.printStackTrace();
-				entity.setResult(StateInfo.FAILED);
 				LOGGER.error("上传失败,{}::", file.getOriginalFilename());
-				return entity;
+				return;
 			}
-
 			//上传成功后，保存pic信息
 			String upShowPath = Constants.localShowPath + fileName;
-
 			picService.addPic(desc, upShowPath);
-			entity.setResult(StateInfo.SUCCESS);
 		}
-		entity.setResult(StateInfo.FAILED);
-		return entity;
+		return;
 	}
 
 
