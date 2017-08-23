b04d5da71df357db6785c1a73a15629cd35d4b1d
ZaneyYe <mia5121@163.com>
2017-08-23 14:12:58 +0800

    user manager

--- src/main/java/com/jinxin/manager/controller/UserController.java
+++ src/main/java/com/jinxin/manager/controller/UserController.java
@@ -1,7 +1,16 @@
 package com.jinxin.manager.controller;
 
+import com.jinxin.manager.enumkit.StateInfo;
+import com.jinxin.manager.vo.ResponseEntity;
+import com.jinxin.manager.vo.UserVo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by yzy on 2017/08/18 下午 3:51.
@@ -9,8 +18,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * 用户管理接口
  */
 @Controller
-@RequestMapping("/")
+@RequestMapping("/user")
 public class UserController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+
+	@RequestMapping(value = "/addUser.do", method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseEntity<StateInfo> addUser(HttpServletRequest request,
+											 UserVo userVo) {
+		LOGGER.info("增加用户...name::{}", userVo.getName());
+		ResponseEntity<StateInfo> responseEntity = new ResponseEntity<>();
+		responseEntity.setResult(StateInfo.SUCCESS);
+		return responseEntity;
+	}
+
 
 
 }
---
+++ src/main/java/com/jinxin/manager/vo/UserVo.java
@@ -0,0 +1,11 @@
+package com.jinxin.manager.vo;
+
+import com.jinxin.manager.po.User;
+
+/**
+ * Created by yzy on 2017/08/22 下午 4:30.
+ * email: mia5121@163.com
+ */
+public class UserVo extends User {
+
+}
