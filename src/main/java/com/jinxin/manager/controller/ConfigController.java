eb78f871743e9ca2cc1df0010783ce856a66df32
yezhangyuan <mia5121@163.com>
2017-12-04 18:29:56 +0800

    undo

---
+++ src/main/java/com/jinxin/manager/controller/ConfigController.java
@@ -0,0 +1,42 @@
+package com.jinxin.manager.controller;
+
+import com.baidu.ueditor.ActionEnter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by yzy yezhangyuan on 2017/12/4 0004.
+ *
+ * @author yezhangyuan
+ */
+@Controller
+@RequestMapping("/config")
+public class ConfigController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ConfigController.class);
+
+	@RequestMapping(value = "/ueditor", method = RequestMethod.GET)
+	@ResponseBody
+	public String ueUpload(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+		request.setCharacterEncoding("utf-8");
+		response.setHeader("Content-Type", "text/html");
+		String roolPath = request.getSession().getServletContext().getRealPath("/");
+		//roolPath:E:\IdeaProjects\JinXinManager\target\JinXinManager\
+//		String roolPath = ConfigController.class.getClassLoader().getResource("").getPath();
+		LOGGER.info("roolPath:.......{}", roolPath);
+		LOGGER.info("contextPath:..... {}", request.getContextPath());
+		LOGGER.info("request uri:.....{}", request.getRequestURI());
+		String configStr = new ActionEnter(request, roolPath).exec();
+		return configStr;
+	}
+
+
+}
--- src/main/java/com/jinxin/manager/controller/PicController.java
+++ src/main/java/com/jinxin/manager/controller/PicController.java
@@ -7,6 +7,7 @@ import com.jinxin.manager.vo.ImgInfoVo;
 import com.jinxin.manager.vo.PageInfo;
 import com.jinxin.manager.vo.RequestPage;
 import com.jinxin.manager.vo.ResponseEntity;
+import org.apache.commons.lang.math.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
@@ -17,9 +18,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by yzy on 2017/08/10 下午 2:43.
@@ -28,19 +34,20 @@ import java.util.List;
  */
 @RequestMapping("/pic")
 @Controller
-public class PicController extends BaseController{
+public class PicController extends BaseController {
 
 	@Autowired
 	private PicService picService;
 
 	/**
 	 * 图片列表
+	 *
 	 * @param request
 	 * @return
 	 */
 	@RequestMapping(value = "/list.do")
 	@ResponseBody
-	public PageInfo<List<ImgInfoVo>> listPics(HttpServletRequest request, RequestPage page){
+	public PageInfo<List<ImgInfoVo>> listPics(HttpServletRequest request, RequestPage page) {
 		LOGGER.info("pic list loading, {}, {} ....", page.getPage(), page.getRows());
 		PageInfo<List<ImgInfoVo>> pageInfo = picService.queryAllPics(page);
 		return pageInfo;
@@ -59,6 +66,7 @@ public class PicController extends BaseController{
 						  @RequestParam("desc") String desc,
 						  @RequestParam("upFile") MultipartFile file) {
 		LOGGER.info("pic uploading, {}, {}....", desc, file.getOriginalFilename());
+
 		if (!file.isEmpty()) {
 			//上传文件
 			//文件名
@@ -88,6 +96,44 @@ public class PicController extends BaseController{
 		picService.delPic(picId);
 		entity.setResult(StateInfo.SUCCESS);
 		return entity;
+	}
+
+
+	@RequestMapping(value = "ue/uploadPic.do", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> images(MultipartFile upfile, HttpServletRequest request, HttpServletResponse response) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		try {
+			String basePath = Constants.localUploadPath;
+			String visitUrl = Constants.localShowPath;
+			if (basePath == null || "".equals(basePath)) {
+				basePath = "E:\\JinXin\\pics\\";  //与properties文件中lyz.uploading.url相同，未读取到文件数据时为basePath赋默认值
+			}
+			if (visitUrl == null || "".equals(visitUrl)) {
+				visitUrl = "http://localhost/"; //与properties文件中lyz.visit.url相同，未读取到文件数据时为visitUrl赋默认值
+			}
+			String fileName = String.valueOf(System.currentTimeMillis()).concat("_")
+					.concat(String.valueOf(RandomUtils.nextInt())).concat(".")
+					.concat(upfile.getOriginalFilename());
+			StringBuilder sb = new StringBuilder();
+			//拼接保存路径
+			sb.append(basePath).append("/").append(fileName);
+			visitUrl = visitUrl.concat(fileName);
+			File f = new File(sb.toString());
+			if (!f.exists()) {
+				f.getParentFile().mkdirs();
+			}
+			OutputStream out = new FileOutputStream(f);
+			FileCopyUtils.copy(upfile.getInputStream(), out);
+			params.put("state", "SUCCESS");
+			params.put("url", visitUrl);
+			params.put("size", upfile.getSize());
+			params.put("original", fileName);
+			params.put("type", upfile.getContentType());
+		} catch (Exception e) {
+			params.put("state", "ERROR");
+		}
+		return params;
 	}
 
 
